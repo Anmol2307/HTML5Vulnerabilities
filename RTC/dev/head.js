0781f9d3df4807c029cb5015de239721b86539bc
Vibhor Kanojia <versatilevibhor@gmail.com>
2015-05-08 16:40:55 +0530

    Post final push

---
+++ RTC/dev/head.js
@@ -0,0 +1,26 @@
+// Last time updated at Feb 07, 2015, 08:32:23
+
+// Quick-Demo for newbies: http://jsfiddle.net/c46de0L8/
+// Another simple demo: http://jsfiddle.net/zar6fg60/
+
+// Latest file can be found here: https://cdn.webrtc-experiment.com/RTCMultiConnection.js
+
+// Muaz Khan     - www.MuazKhan.com
+// MIT License   - www.WebRTC-Experiment.com/licence
+// Documentation - www.RTCMultiConnection.org/docs
+// FAQ           - www.RTCMultiConnection.org/FAQ
+// Changes log   - www.RTCMultiConnection.org/changes-log/
+// Demos         - www.WebRTC-Experiment.com/RTCMultiConnection
+
+// _________________________
+// RTCMultiConnection-v2.2.5
+
+/* issues/features need to be fixed & implemented:
+
+-. v2.2.* changes-log here: http://www.rtcmulticonnection.org/changes-log/#v2.2
+-. Fixed: https://github.com/muaz-khan/WebRTC-Experiment/issues/321
+*/
+
+'use strict';
+
+(function() {
